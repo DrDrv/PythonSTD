97069186afcde3b042aaa5168052c7e36443ea76
DrDrv <dzen.driver@gmail.com>
2022-05-19 22:13:34 +0300

    3

---
+++ Lesson/less1.py
@@ -0,0 +1,28 @@
+a = 123
+b = 1.23
+#print(type(a))
+print(type(b))
+
+c = 'hello world'
+s = "hello world" # \n  - новая строка
+print(s)
+
+print(a,'-', b,'-', c)
+print('{} - {} - {}'.format(a,b,c))
+print('{1} - {2} - {0}'.format(a,b,c))
+
+f = True
+
+list = [1,2,3]
+print(list)
+
+
+print('input a')
+a = input() #int(input())   float(input())
+print(a)
+
+a = 192
+b = -65
+c = a / b # деление в целых числах //, в степень **,  %  остаток, round(a*b, 5) 5 число знаков - 
+# a = a + 5 a+=5
+# 

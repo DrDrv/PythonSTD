2e05e67787efea84eb79b3ca1466e47bd245c297
Drv <dzen.driver@gmail.com>
2022-05-29 22:47:55 +0300

    task 1-3 Ready

--- DZ/dz3.py
+++ DZ/dz3.py
@@ -18,8 +18,44 @@ print(f'Для чисел {number_a} и {number_b} НОК  =  {multi_nok(number_
 #     Пример: при d = 0.001,  c= 3.141.
 
 print('---= Задача 2 =---')
+def number_pi(delta):
+    num_cols = 1 # Номер члена послесдовательности расчета числа Пи
+    num_pi = 0  # Число Пи
+    element = 1 # значение элемента последовательности
+    while element >= delta:
+        element = 1/(2*num_cols -1)
+        if num_cols%2 == 0: num_pi -= element
+        else: num_pi += element
+        num_cols += 1
+    num_pi *= 4
+    return (num_pi)
 
-n = 5
-a = 1.2345678912345
-template = '{:.' + str(n) + 'f}'
-print(template.format(a))
+print(number_pi(0.001))
+
+# Составить список простых множителей натурального числа N
+print('---= Задача 3 =---')
+number_N = 26
+print(f'простые множители для числа {number_N}')
+
+def simple_mult(N):
+    if N in [2,3,5,7]:
+        return N
+    if N%2 != 0 and N%3 and N%5 != 0 and N%7 != 0:
+        if (pow(N,1/2))%1 == 0:
+            print(pow(N,1/2))
+            return simple_mult(int(pow(N,1/2)))
+        return N
+    if N%2 == 0: return simple_mult(N/2)
+    if N%3 == 0: return simple_mult(N/3)
+    if N%5 == 0: return simple_mult(N/5)
+    if N%7 == 0: return simple_mult(N/7)
+
+list = []
+while number_N != 1:
+    funk_rez = int(simple_mult(number_N))
+    list.append(funk_rez)
+    number_N = (number_N/funk_rez)
+print(list)
+
+
+

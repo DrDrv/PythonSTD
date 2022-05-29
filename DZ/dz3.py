902d5a7be6481a5d59d79e2fdd2bbcd02ebb39a5
Drv <dzen.driver@gmail.com>
2022-05-30 00:22:03 +0300

    DZ minimalka ready

--- DZ/dz3.py
+++ DZ/dz3.py
@@ -18,7 +18,7 @@ print(f'Для чисел {number_a} и {number_b} НОК  =  {multi_nok(number_
 #     Пример: при d = 0.001,  c= 3.141.
 
 print('---= Задача 2 =---')
-def number_pi(delta):
+def number_pi(delta):   # дельта это точность
     num_cols = 1 # Номер члена послесдовательности расчета числа Пи
     num_pi = 0  # Число Пи
     element = 1 # значение элемента последовательности
@@ -34,16 +34,14 @@ print(number_pi(0.001))
 
 # Составить список простых множителей натурального числа N
 print('---= Задача 3 =---')
-number_N = 26
+number_N = 12
 print(f'простые множители для числа {number_N}')
 
 def simple_mult(N):
     if N in [2,3,5,7]:
         return N
-    if N%2 != 0 and N%3 and N%5 != 0 and N%7 != 0:
-        if (pow(N,1/2))%1 == 0:
-            print(pow(N,1/2))
-            return simple_mult(int(pow(N,1/2)))
+    if N%2 != 0 and N%3 != 0 and N%5 != 0 and N%7 != 0:
+        if (pow(N,1/2))%1 == 0: return simple_mult(int(pow(N,1/2)))
         return N
     if N%2 == 0: return simple_mult(N/2)
     if N%3 == 0: return simple_mult(N/3)
@@ -57,5 +55,43 @@ while number_N != 1:
     number_N = (number_N/funk_rez)
 print(list)
 
+# Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
+# Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
+print('---= Задача 4 =---')
+list_in = [1, 2, 3, 5, 1, 5, 3, 10]
+print(f'Входящий список {list_in}')
+i = len(list_in)-1          # Длина входящего списка
+while i >= 0:
+    if list_in.count(list_in[i]) > 1:
+        list_in.pop(i)
+    i -= 1
+print(f' Результат {list_in}')
 
+# Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа. 
+print('---= Задача 5 =---')
+import random
+def int_number_list(file_in):
+    N = 10                                  # Число целых цифр
+    with open(file_in,'w') as file:
+        for i in range(N):                  
+            file.write(str(random.randint(0,N-1)) + '\n')
+    print('содержимое входящего файла: ')
+    with open(file_in,'r') as file:
+        for i in file:
+            print(str(int(i))+' ')     
+    return
 
+def del_krat_two(file_in):
+    collection_nechet = []          # список для промежуточного хранения данных
+    with open(file_in,'r') as file:
+        for i in file:
+            if int(i)%2 != 0: collection_nechet.append(str(i))
+    with open(file_in,'w') as file:
+        print('содержимое выходного файла: ')
+        for i in collection_nechet:
+            print(str(int(i))+' ')
+            file.write(str(i))
+    return
+
+int_number_list('file_dz3.txt')
+del_krat_two('file_dz3.txt')

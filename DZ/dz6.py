79218b44a164ce17e3a1b598f9fc78144cd53071
DrDrv <dzen.driver@gmail.com>
2022-06-09 00:43:01 +0300

    DZ6 task 3 ready

--- DZ/dz6.py
+++ DZ/dz6.py
@@ -2,48 +2,48 @@
 # Пример: 2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
 # Дополнительно: Добавить возможность использования скобок, меняющих приоритет операций. 
 # Пример: 1+2*3 => 7; (1+2)*3 => 9;
-from operator import index
+# from operator import index
 
 
-print(' ================================')
-print('Консольный калькулятор')
-print('')
-operand = ['*','/','+','-', '(',')']
-formula = (input(f'Введите выражение: ')).replace(' ','')
+# print(' ================================')
+# print('Консольный калькулятор')
+# print('')
+# operand = ['*','/','+','-', '(',')']
+# formula = (input(f'Введите выражение: ')).replace(' ','')
 
-# Функция разбивает выражение на числа и операторы и закидывает все это в список
-def dev_form(f_la,op_nd):
-    tmp = ''
-    list_f_la = []
-    for i in range(0,len(f_la)):
-        if f_la[i] in op_nd:
-            if tmp != '': list_f_la.append(tmp)
-            tmp = ''
-            list_f_la.append(f_la[i])
-        else: 
-            tmp += f_la[i]
-    if tmp != '':list_f_la.append(tmp)
-    return list_f_la
+# # Функция разбивает выражение на числа и операторы и закидывает все это в список
+# def dev_form(f_la,op_nd):
+#     tmp = ''
+#     list_f_la = []
+#     for i in range(0,len(f_la)):
+#         if f_la[i] in op_nd:
+#             if tmp != '': list_f_la.append(tmp)
+#             tmp = ''
+#             list_f_la.append(f_la[i])
+#         else: 
+#             tmp += f_la[i]
+#     if tmp != '':list_f_la.append(tmp)
+#     return list_f_la
 
-def calc(f_la, op_nd):
-    new_formula = []
-    for i in f_la:
-        if i == '*':
-            k = f_la.index(i)
-            new_formula.append(int(f_la[k-1]) * int(f_la[k+1]))
-        if i == '/':
-            k = form.index(i)
-            new_formula.append(int(f_la[k-1]) / int(f_la[k+1]))
-        if i == '+':
-            k = form.index(i)
-            new_formula.append(int(f_la[k-1]) + int(f_la[k+1]))
-        if i == '-':
-            k = form.index(i)
-            new_formula.append(int(f_la[k-1]) - int(f_la[k+1]))
+# def calc(f_la, op_nd):
+#     new_formula = []
+#     for i in f_la:
+#         if i == '*':
+#             k = f_la.index(i)
+#             new_formula.append(int(f_la[k-1]) * int(f_la[k+1]))
+#         if i == '/':
+#             k = form.index(i)
+#             new_formula.append(int(f_la[k-1]) / int(f_la[k+1]))
+#         if i == '+':
+#             k = form.index(i)
+#             new_formula.append(int(f_la[k-1]) + int(f_la[k+1]))
+#         if i == '-':
+#             k = form.index(i)
+#             new_formula.append(int(f_la[k-1]) - int(f_la[k+1]))
 
-    rez = 0
-    return rez
-print(dev_form(formula,operand))
+#     rez = 0
+#     return rez
+# print(dev_form(formula,operand))
 #print(str(new_formula))
 
 # 2 - Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных файлах (в одном файлике отрывок из какой-то книги, а втором файлике — сжатая версия этого текста). 
@@ -113,3 +113,28 @@ print(dev_form(formula,operand))
 # Создайте функцию, которая принимает строку и возвращает строку, зашифрованную с помощью Rot13 . Если в строку включены числа или специальные символы, они должны быть
 # возвращены как есть. Также создайте функцию, которая расшифровывает эту строку обратно (некий начальный аналог шифрования сообщений). 
 # Не использовать функцию encode.
+# print(' ================================')
+# print(' Шифрование ROT 13')
+# print('')
+# text = 'This is an encrypted message.'
+# alphavit = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ]
+             
+
+# print(f'Исходный текст: {text}')
+
+# def encodecod(txt,al_t):
+#     data = txt
+#     for i in al_t:
+#         for k in range(len(txt)):
+#             if i == txt[k]:
+#                 if (al_t.index(i) > 0 and al_t.index(i) < 13) or (al_t.index(i) > 25 and al_t.index(i) < 39):
+#                     data = data[:k] + al_t[al_t.index(i)+13] + data[k+1:]
+#                 if (al_t.index(i) > 12 and al_t.index(i) < 26) or (al_t.index(i) > 38 and al_t.index(i) < 52):
+#                     data = data[:k] + al_t[al_t.index(i)-13] + data[k+1:]
+#     return data
+
+# en_text = encodecod(text,alphavit)
+# print(f'Зашифрованное сообщение: {en_text}')
+
+# en_text = encodecod(en_text,alphavit)
+# print(f'Расшифрованное сообщение: {en_text}')
